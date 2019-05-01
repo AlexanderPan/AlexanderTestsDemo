4262831ef6b68c20dc24e94006362dd5a8920586
AlexanderPan <Alexander.pan@ezprice.com.tw>
2019-05-01 11:47:19 +0800

    maek uitest

--- AlexanderTestsDemoUITests/AlexanderTestsDemoUITests.swift
+++ AlexanderTestsDemoUITests/AlexanderTestsDemoUITests.swift
@@ -24,40 +24,40 @@ class AlexanderTestsDemoUITests: XCTestCase {
     }
     
     
-    func testWhen_API_Error_Should_Show_Error_Message() {
-    
-        self.app.launchEnvironment = ["contactsListAPI(request:)":FakeData.api_contactsListAPI_error() , "queryContacts()":FakeData.db_queryContacts_success()]
-        self.app.launch()
-        let progress = app.otherElements["api error (999)"]
-        let exists = NSPredicate(format: "exists == 1")
-        expectation(for: exists, evaluatedWith: progress, handler: nil)
-        waitForExpectations(timeout: 5, handler: nil)
-
-    }
-    
-    
-    func testWhen_User_Click_Cell_Then_Go_ContactsViewController_Assert_Info() {
-      
-        self.app.launchEnvironment = ["contactsListAPI(request:)":FakeData.api_contactsListAPI_success() , "queryContacts()":FakeData.db_queryContacts_success()]
-        self.app.launch()
-        let cells = self.app.tables.cells
-        let exists = NSPredicate(format: "count == 5")
-        expectation(for: exists, evaluatedWith: cells, handler: nil)
-        waitForExpectations(timeout: 5, handler: nil)
-        let cell0 = cells.element(boundBy: 0)
-        XCTAssertTrue(cell0.images["icon1"].exists)
-        XCTAssertTrue(cell0.staticTexts["A - UI測試複製人"].exists)
-        print("\(self.app.debugDescription)")
-        
-
-        
-        cell0.tap()
-        XCTAssertTrue(self.app.navigationBars["聯絡人詳細資料"].exists)
-        XCTAssertTrue(self.app.staticTexts["A - UI測試複製人"].exists)
-        XCTAssertTrue(self.app.staticTexts["11"].exists)
-        XCTAssertTrue(self.app.staticTexts["0911111112"].exists)
-        XCTAssertTrue(self.app.images["icon1"].exists)
-        
-    }
+//    func testWhen_API_Error_Should_Show_Error_Message() {
+//
+//        self.app.launchEnvironment = ["contactsListAPI(request:)":FakeData.api_contactsListAPI_error() , "queryContacts()":FakeData.db_queryContacts_success()]
+//        self.app.launch()
+//        let progress = app.otherElements["api error (999)"]
+//        let exists = NSPredicate(format: "exists == 1")
+//        expectation(for: exists, evaluatedWith: progress, handler: nil)
+//        waitForExpectations(timeout: 5, handler: nil)
+//
+//    }
+//
+//
+//    func testWhen_User_Click_Cell_Then_Go_ContactsViewController_Assert_Info() {
+//
+//        self.app.launchEnvironment = ["contactsListAPI(request:)":FakeData.api_contactsListAPI_success() , "queryContacts()":FakeData.db_queryContacts_success()]
+//        self.app.launch()
+//        let cells = self.app.tables.cells
+//        let exists = NSPredicate(format: "count == 5")
+//        expectation(for: exists, evaluatedWith: cells, handler: nil)
+//        waitForExpectations(timeout: 5, handler: nil)
+//        let cell0 = cells.element(boundBy: 0)
+//        XCTAssertTrue(cell0.images["icon1"].exists)
+//        XCTAssertTrue(cell0.staticTexts["A - UI測試複製人"].exists)
+//        print("\(self.app.debugDescription)")
+//
+//
+//
+//        cell0.tap()
+//        XCTAssertTrue(self.app.navigationBars["聯絡人詳細資料"].exists)
+//        XCTAssertTrue(self.app.staticTexts["A - UI測試複製人"].exists)
+//        XCTAssertTrue(self.app.staticTexts["11"].exists)
+//        XCTAssertTrue(self.app.staticTexts["0911111112"].exists)
+//        XCTAssertTrue(self.app.images["icon1"].exists)
+//
+//    }
 
 }
